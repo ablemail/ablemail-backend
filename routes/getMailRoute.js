1b7a135be2cf52cbdb2c58f130ad0e854255e2f2
Tristan Peng <pengt@catlin.edu>
2020-02-15 21:50:32 -0800

    Exchange id for message

--- routes/getMailRoute.js
+++ routes/getMailRoute.js
@@ -7,13 +7,22 @@ const { user } = require('./authRoutes');
 
 router.get('/', verifyKey, async (req, res) => {
   const email = (await user).email;
+  const token = await accessToken;
   const data = await axios.get(`https://www.googleapis.com/gmail/v1/users/${ email }/messages?key=${ google.clientSecret }`, {
     headers: {
       Accept: 'application/json',
-      Authorization: `Bearer ${ await accessToken }`
+      Authorization: `Bearer ${ token }`
     }
   });
-  res.json(data.data.messages);
+
+  let messages = [];
+  data.data.messages.forEach(message => messages.push(axios.get(`https://www.googleapis.com/gmail/v1/users/${ email }/messages/${ message.id }?key=${ google.clientSecret }`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${ token }`
+    }
+  })));
+  res.json(messages);
 });
 
 module.exports = router;
