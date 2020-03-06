3cef2133bd029e58603c36f0931a46de04f5c3b4
Tristan Peng <pengt@catlin.edu>
2020-03-05 23:01:06 -0800

    Return non-empty body

--- routes/sendRoute.js
+++ routes/sendRoute.js
@@ -4,11 +4,12 @@ const { createTransport, sendMail } = require('../helper/nodemailer');
 
 router.post('/', async (req, res) => { // TODO: Add key
   const transport = await createTransport(req.body.id);
-  sendMail(req.body.id, transport, {
+  const { sent } = await sendMail(req.body.id, transport, {
     to: req.body.to,
     subject: req.body.subject,
     text: req.body.body // TODO: Style with HTML
   });
+  res.json({ sent });
 });
 
 module.exports = router;
